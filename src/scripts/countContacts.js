13e7606058fc4deac12a7c6017b6d637c2b11e72
Tinadin <pitynia8717@gmail.com>
2025-05-28 01:53:28 +0300

    Initial commit

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,11 @@
-export const countContacts = async () => {};
+// export const countContacts = async () => {};
+
+// console.log(await countContacts());
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  const contactsList = await readContacts();
+  return contactsList.length;
+};
 
 console.log(await countContacts());
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,15 @@
-const generateContacts = async (number) => {};
+// const generateContacts = async (number) => {};
+
+// generateContacts(5);
+
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactsList = await readContacts();
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  await writeContacts([...contactsList, ...newContacts]);
+};
 
 generateContacts(5);
